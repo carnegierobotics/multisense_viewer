9c0856690a7531b1a208e7e683c3c9aedc0825c1
Magnus Kaufmann Gjerde <gjerde.magnus@gmail.com>
2022-07-15 07:36:50 +0200

    Autoconnect working on Windows and returns a multisense::Channel object

--- experimental_features/AutoConnect/AutoConnect.h
+++ experimental_features/AutoConnect/AutoConnect.h
@@ -5,14 +5,35 @@
 #ifndef AUTOCONNECT_AUTOCONNECT_H
 #define AUTOCONNECT_AUTOCONNECT_H
 
+#define MAX_CONNECTION_ATTEMPTS 4
+#define TIMEOUT_INTERVAL_SECONDS 6
 
-#include <MultiSense/MultiSenseTypes.hh>
+#include <include/MultiSense/MultiSenseTypes.hh>
+#include "MultiSense/MultiSenseChannel.hh"
+
+#include <string>
+#include <vector>
+#include <thread>
 
 class AutoConnect {
-
 public:
+
+    enum FoundCameraOnIp {
+        FOUND_CAMERA = 1,
+        NO_CAMERA_RETRY = 0,
+        NO_CAMERA = -1
+    };
+
+    struct Result {
+        std::string cameraIpv4Address;
+        std::string networkAdapter;
+        std::string networkAdapterLongName;
+    }result;
+
     struct AdapterSupportResult {
         std::string name; // Name of network adapter tested
+        std::string description = "No description available";
+        std::string lName;
         bool supports; // 0: for bad, 1: for good
 
         AdapterSupportResult(const char *name, uint8_t supports) {
@@ -21,10 +42,16 @@ public:
         }
     };
 
+    bool success = false;
+    bool loopAdapters = true;
+    bool listenOnAdapter = true;
+    time_t startTime;
+    crl::multisense::Channel* cameraInterface;
+
     virtual std::vector<AdapterSupportResult> findEthernetAdapters() = 0;
     virtual void start(std::vector<AdapterSupportResult> vector) = 0;
     virtual void onFoundAdapters(std::vector<AdapterSupportResult> vector) = 0;
-    virtual bool onFoundIp(std::string string, AdapterSupportResult adapter) = 0;
+    virtual FoundCameraOnIp onFoundIp(std::string string, AdapterSupportResult adapter) = 0;
     virtual void onFoundCamera() = 0;
     virtual void stop() = 0;
 
@@ -41,6 +68,9 @@ private:
         int sensorMTU = 0;
 
     } cameraInfo;
+protected:
+    std::thread *t{};
+    int connectAttemptCounter = 0;
 
 };
 

a0234862e127565d3ce4b8efcdebd8067998889a
magnus <gjerde.magnus@gmail.com>
2023-01-10 18:36:33 +0100

    Disabled GUI and added a S27 model to test pbr

--- src/ImGui/GuiManager.cpp
+++ src/ImGui/GuiManager.cpp
@@ -102,7 +102,7 @@ namespace VkRender {
 
         {
             for (auto &layer: m_LayerStack) {
-                layer->onUIRender(&handles);
+                //layer->onUIRender(&handles);
 
             }
         }

d9440db5e9b9aa56962f94011fcd1c9b81536b35
magnus <gjerde.magnus@gmail.com>
2023-02-01 11:15:54 +0100

    reset Camera.h

--- include/Viewer/Core/Camera.h
+++ include/Viewer/Core/Camera.h
@@ -92,9 +92,9 @@ public:
     CameraType type = CameraType::flycam;
 
     glm::vec3 m_Rotation = glm::vec3();
-    glm::vec3 m_Position = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 m_Position = glm::vec3(1.0f, 0.0f, 3.0f);
     glm::vec4 m_ViewPos = glm::vec4();
-    glm::vec3 m_FocalPoint = glm::vec3(0.0f);
+    glm::vec3 m_FocalPoint =glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
     glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 

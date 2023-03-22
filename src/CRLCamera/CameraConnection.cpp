1589ebc3fa86d3c06fdb2cac60575da6dfe92397
magnus <gjerde.magnus@gmail.com>
2023-03-22 10:11:36 +0100

    disabled bicubic interpolation

--- src/CRLCamera/CameraConnection.cpp
+++ src/CRLCamera/CameraConnection.cpp
@@ -627,7 +627,8 @@ namespace VkRender::MultiSense {
     CameraConnection::~CameraConnection() {
         // Make sure delete the camPtr for physical cameras so we run destructor on the physical camera class which
         // stops all streams on the camera
-        pool->Stop();
+        if (pool)
+            pool->Stop();
 #ifndef WIN32
         if (m_FD != -1)
             close(m_FD);

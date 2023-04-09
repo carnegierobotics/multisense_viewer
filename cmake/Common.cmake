26a56fafd5a8b23201bba8673323e4384ee0eaab
magnus <gjerde.magnus@gmail.com>
2023-04-09 21:33:47 +0200

    WIP usage statistics

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -175,6 +175,9 @@ function(ExportScriptIncludes)
     endforeach (Src ${SCRIPT_HEADERS})
 endfunction()
 
+function (GenerateVersionFile)
+    file(WRITE ${CMAKE_SOURCE_DIR}/Assets/Generated/versioninfo "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
+endfunction()
 
 if (UNIX)
     set(INSTALL_DIRECTORY ${CMAKE_BINARY_DIR}/multisense_${VERSION_MAJOR}.${VERSION_MINOR}-${VERSION_PATCH}_${ARCHITECTURE}/opt/multisense)

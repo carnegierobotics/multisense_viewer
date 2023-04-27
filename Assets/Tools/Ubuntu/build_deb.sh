8715e0db3b1e4e1c01d79a996cc75238807760b5
magnus <gjerde.magnus@gmail.com>
2023-04-27 20:44:52 +0200

    link with ssl and crypto

--- Assets/Tools/Ubuntu/build_deb.sh
+++ Assets/Tools/Ubuntu/build_deb.sh
@@ -1,14 +1,14 @@
 # Run from build folder
 echo "Make sure to run this from a {source}/build folder"
-cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build . --config Release --target install
-mkdir multisense_1.0-0_amd64/DEBIAN
-mkdir -p multisense_1.0-0_amd64/usr/share/applications
-cp ../Assets/Tools/Ubuntu/start.sh multisense_1.0-0_amd64/opt/multisense/
-cp ../Assets/Tools/Ubuntu/multisense.desktop multisense_1.0-0_amd64/usr/share/applications/
-cp ../Assets/Tools/Ubuntu/control multisense_1.0-0_amd64/DEBIAN
-rm -rf multisense_1.0-0_amd64/opt/multisense/include
-rm -rf multisense_1.0-0_amd64/opt/multisense/lib
-rm -rf multisense_1.0-0_amd64/opt/multisense/share
-rm -rf multisense_1.0-0_amd64/opt/multisense/bin
-rm -rf multisense_1.0-0_amd64/opt/multisense/Assets/Tools
-dpkg-deb --build --root-owner-group multisense_1.0-0_amd64
+cmake .. -DCMAKE_BUILD_TYPE=Release && cmake --build . --config Release --target install -- -j12
+mkdir multisense_1.1-0_amd64/DEBIAN
+mkdir -p multisense_1.1-0_amd64/usr/share/applications
+cp ../Assets/Tools/Ubuntu/start.sh multisense_1.1-0_amd64/opt/multisense/
+cp ../Assets/Tools/Ubuntu/multisense.desktop multisense_1.1-0_amd64/usr/share/applications/
+cp ../Assets/Tools/Ubuntu/control multisense_1.1-0_amd64/DEBIAN
+rm -rf multisense_1.1-0_amd64/opt/multisense/include
+rm -rf multisense_1.1-0_amd64/opt/multisense/lib
+rm -rf multisense_1.1-0_amd64/opt/multisense/share
+rm -rf multisense_1.1-0_amd64/opt/multisense/bin
+rm -rf multisense_1.1-0_amd64/opt/multisense/Assets/Tools
+dpkg-deb --build --root-owner-group multisense_1.1-0_amd64

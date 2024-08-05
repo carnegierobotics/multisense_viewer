1ab3cfe8d35662e231bd5e252e2b75d651bf2a1b
magnus <gjerde.magnus@gmail.com>
2024-08-05 16:53:55 +0200

    simple .obj file viewport loading

--- src/Viewer/Scenes/Default/DefaultScene.cpp
+++ src/Viewer/Scenes/Default/DefaultScene.cpp
@@ -15,7 +15,8 @@ namespace VkRender {
 
 
         auto entity = m_context.createEntity("FirstEntity");
-        auto &modelComponent = entity.addComponent<VkRender::OBJModelComponent>(Utils::getModelsPath() / "obj" / "s30.obj", &m_context.vkDevice());
+        auto &modelComponent = entity.addComponent<VkRender::OBJModelComponent>(
+                Utils::getModelsPath() / "obj" / "s30.obj", &m_context.vkDevice());
         //auto &res = entity.addComponent<VkRender::DefaultGraphicsPipelineComponent2>(&m_context->data(),);
 
 
@@ -55,11 +56,9 @@ namespace VkRender {
     }
 
     void DefaultScene::addGuiLayers() {
-
     }
 
     void DefaultScene::render(CommandBuffer &drawCmdBuffers) {
-        uint32_t currentFrame = *drawCmdBuffers.frameIndex;
         /*
         auto sky = m_context.findEntityByName("Skybox");
         if (sky)
@@ -69,14 +68,11 @@ namespace VkRender {
             modelComponent.getComponent<VkRender::GLTFModelComponent>().model->draw(drawCmdBuffers.buffers[currentFrame]);
 
          */
-
         auto entity = m_context.findEntityByName("FirstEntity");
-        if (entity){
+        if (entity) {
             auto &resources = entity.getComponent<DefaultGraphicsPipelineComponent>();
             resources.draw(drawCmdBuffers);
-
         }
-
     }
 
     void DefaultScene::update() {
@@ -88,26 +84,16 @@ namespace VkRender {
 
         auto skybox = m_context.findEntityByName("Skybox");
         if (skybox) {
-            /*
-            auto &obj = skybox.getComponent<VkRender::SkyboxGraphicsPipelineComponent>();
-            // Skybox
-            obj.uboMatrix.projection = camera.matrices.perspective;
-            obj.uboMatrix.model = glm::mat4(glm::mat3(camera.matrices.view));
-            obj.uboMatrix.model = glm::rotate(obj.uboMatrix.model, glm::radians(90.0f),
-                                              glm::vec3(1.0, 0.0, 0.0)); // z-up rotation
+        }
 
-            obj.update();
-*/
-
-            auto entity = m_context.findEntityByName("FirstEntity");
-            if (entity){
-                    auto &resources = entity.getComponent<DefaultGraphicsPipelineComponent>();
-                    auto &transform = entity.getComponent<TransformComponent>();
-                    transform.scale = glm::vec3(0.6f, 0.6f, 0.6f);
-                    resources.updateTransform(transform);
-                    resources.updateView(camera);
-                    resources.update(m_context.currentFrameIndex());
-            }
+        auto entity = m_context.findEntityByName("FirstEntity");
+        if (entity) {
+            auto &resources = entity.getComponent<DefaultGraphicsPipelineComponent>();
+            auto &transform = entity.getComponent<TransformComponent>();
+            transform.scale = glm::vec3(0.6f, 0.6f, 0.6f);
+            resources.updateTransform(transform);
+            resources.updateView(camera);
+            resources.update(m_context.currentFrameIndex());
         }
 
 
@@ -116,7 +102,6 @@ namespace VkRender {
             auto &script = view.get<ScriptComponent>(entity);
             script.script->update();
         }
-
     }
 
 

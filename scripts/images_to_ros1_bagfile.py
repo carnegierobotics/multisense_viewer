97b9c08ad4cdd03539a9bb8cb27ad22c860799fd
Matt Alvarado <malvarado@carnegierobotics.com>
2024-10-14 13:01:27 -0400

    Updates to support creating bag files with aux data

--- scripts/images_to_ros1_bagfile.py
+++ scripts/images_to_ros1_bagfile.py
@@ -21,7 +21,7 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 from std_msgs.msg import Header
 
-def write_bag_file(bag_path, image_paths, left_camera_info, right_camera_info):
+def write_bag_file(bag_path, image_paths, left_camera_info, right_camera_info, aux_camera_info):
     bag = rosbag.Bag(bag_path, 'w')
 
     bridge = CvBridge()
@@ -33,12 +33,19 @@ def write_bag_file(bag_path, image_paths, left_camera_info, right_camera_info):
         if 'right' in topic:
             camera_info_base = right_camera_info
             frame = "right_optical"
+        elif 'aux' in topic:
+            camera_info_base = aux_camera_info
+            frame = "aux_optical"
+
 
         with open(timestamps, 'r') as f:
             reader = csv.reader(f, delimiter=',')
             for name, time in reader:
                 image_path = os.path.join(path, name)
-                image = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH)
+                if 'aux' in topic:
+                    image = cv2.imread(image_path)
+                else:
+                    image = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH)
 
                 sec, microsec = time.split('.')
                 microsec = microsec.zfill(6)
@@ -48,7 +55,6 @@ def write_bag_file(bag_path, image_paths, left_camera_info, right_camera_info):
                 header = Header()
                 header.frame_id = frame
                 header.stamp = ros_time
-
 
                 ros_image = bridge.cv2_to_imgmsg(image, "passthrough")
                 ros_image.header = header
@@ -66,6 +72,7 @@ def write_bag_file(bag_path, image_paths, left_camera_info, right_camera_info):
 def get_camera_info(root_images_directory):
     left_camera_info = CameraInfo()
     right_camera_info = CameraInfo()
+    aux_camera_info = CameraInfo()
     for path in [os.path.join(root_images_directory, p) for p in os.listdir(root_images_directory)]:
         if path.endswith("intrinsics.yml"):
             fs = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
@@ -75,6 +82,12 @@ def get_camera_info(root_images_directory):
             right_camera_info.K = fs.getNode("M2").mat().flatten().tolist()
             right_camera_info.D = fs.getNode("D2").mat().flatten().tolist()
 
+            try:
+                aux_camera_info.K = fs.getNode("M3").mat().flatten().tolist()
+                aux_camera_info.D = fs.getNode("D3").mat().flatten().tolist()
+            except cv2.error:
+                print("no aux intrinsic information")
+
         if path.endswith("extrinsics.yml"):
             fs = cv2.FileStorage(path, cv2.FILE_STORAGE_READ)
             left_camera_info.P = fs.getNode("P1").mat().flatten().tolist()
@@ -83,10 +96,17 @@ def get_camera_info(root_images_directory):
             right_camera_info.P = fs.getNode("P2").mat().flatten().tolist()
             right_camera_info.R = fs.getNode("R2").mat().flatten().tolist()
 
+            try:
+                aux_camera_info.P = fs.getNode("P3").mat().flatten().tolist()
+                aux_camera_info.R = fs.getNode("R3").mat().flatten().tolist()
+            except cv2.error:
+                print("no aux extrinsic information")
+
     left_camera_info.distortion_model = 'rational_polynomial' if len(left_camera_info.D) == 8 else 'plumb_bob'
     right_camera_info.distortion_model = 'rational_polynomial' if len(right_camera_info.D) == 8 else 'plumb_bob'
+    aux_camera_info.distortion_model = 'rational_polynomial' if len(aux_camera_info.D) == 8 else 'plumb_bob'
 
-    return left_camera_info, right_camera_info
+    return left_camera_info, right_camera_info, aux_camera_info
 
 def parse_image_directories(root_images_directory, topic_prefix):
     paths = []
@@ -95,7 +115,7 @@ def parse_image_directories(root_images_directory, topic_prefix):
         timestamps_path = None
         topic = None
         for d in dirs:
-            if d == "png" or d == "tiff":
+            if d == "png" or d == "tiff" or d == "ppm":
                 image_path = os.path.join(root, d)
                 topic = topic_prefix + "/" + os.path.basename(root).lower()
                 break
@@ -126,9 +146,9 @@ def main(root_images_directory, output_bag_file, topic_prefix):
         raise RuntimeError('Invalid root directory')
 
     image_paths = parse_image_directories(root_images_directory, topic_prefix)
-    left_camera_info, right_camera_info = get_camera_info(root_images_directory)
+    left_camera_info, right_camera_info, aux_camera_info = get_camera_info(root_images_directory)
 
-    write_bag_file(output_bag_file, image_paths, left_camera_info, right_camera_info)
+    write_bag_file(output_bag_file, image_paths, left_camera_info, right_camera_info, aux_camera_info)
 
 if __name__ == "__main__":
    root_images_directory, output_bag_file, topic_prefix = parse_arguments()

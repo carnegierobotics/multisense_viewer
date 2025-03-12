f8cb66844f5cf1a4288aa81b72d51c86ac40ac5f
magnus <gjerde.magnus@gmail.com>
2025-03-12 15:29:41 +0100

    mesh versioning

--- src/Viewer/Rendering/MeshParameters.cpp
+++ src/Viewer/Rendering/MeshParameters.cpp
@@ -23,39 +23,69 @@ namespace VkRender {
 
     std::shared_ptr<MeshData> QuadricMeshParameters::generateMeshData() {
         // Generate mesh data for a cylinder
+        uint32_t version = 0;
+        if (m_meshData) {
+            version = m_meshData->version;
+        }
+
         auto meshData = std::make_shared<MeshData>();
         meshData->generateQuadricMesh(*this);
         m_meshData = meshData.get();
+        m_meshData->version = ++version;
         return meshData;
     }
 
     std::shared_ptr<MeshData> CameraGizmoPerspectiveMeshParameters::generateMeshData() {
         // Generate mesh data for camera gizmo
+        uint32_t version = 0;
+        if (m_meshData) {
+            version = m_meshData->version;
+        }
+
         auto meshData = std::make_shared<MeshData>();
         meshData->generateCameraPerspectiveGizmoMesh(*this);
         m_meshData = meshData.get();
+        m_meshData->version = ++version;
         return meshData;
     }
 
     std::shared_ptr<MeshData> CameraGizmoPinholeMeshParameters::generateMeshData() {
         // Generate mesh data for camera gizmo
+        uint32_t version = 0;
+        if (m_meshData) {
+            version = m_meshData->version;
+        }
+
         auto meshData = std::make_shared<MeshData>();
         meshData->generateCameraPinholeGizmoMesh(*this);
         m_meshData = meshData.get();
+        m_meshData->version = ++version;
         return meshData;
     }
 
     std::shared_ptr<MeshData> OBJFileMeshParameters::generateMeshData() {
         auto meshData = std::make_shared<MeshData>();
+        uint32_t version = 0;
+        if (m_meshData) {
+            version = m_meshData->version;
+        }
+
         meshData->generateOBJMesh(*this);
         m_meshData = meshData.get();
+        m_meshData->version = ++version;
         return meshData;
     }
 
     std::shared_ptr<MeshData> PLYFileMeshParameters::generateMeshData() {
         auto meshData = std::make_shared<MeshData>();
+        uint32_t version = 0;
+        if (m_meshData) {
+            version = m_meshData->version;
+        }
+
         meshData->generatePLYMesh(*this);
         m_meshData = meshData.get();
+        m_meshData->version = ++version;
         return meshData;
     }
 }
